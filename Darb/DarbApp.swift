041c9db53bb9bd1262083d6864b70db6eae73bbe
hosam <amor_boy20102060@yahoo.com>
2022-10-31 17:37:14 +0200

    Initial Commit

---
+++ Darb/DarbApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarbApp.swift
+//  Darb
+//
+//  Created by hosam on 31/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct DarbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
