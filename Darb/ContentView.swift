0343508af29cfd14f2d5794d4cbf351ad677499e
hosam <amor_boy20102060@yahoo.com>
2022-11-02 04:25:29 +0200

    حخي شييثي

--- Darb/ContentView.swift
+++ Darb/ContentView.swift
@@ -1,21 +1,12 @@
-//
-//  ContentView.swift
-//  Darb
-//
-//  Created by hosam on 31/10/2022.
-//
+
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        StudentStage()
+//        SHomeTabBar()
+//        SchoolStage()
     }
 }
 
